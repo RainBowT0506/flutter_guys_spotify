97ad3af6958fb67f25ea4e83471e6f9907c0d0b3
RainBowT0506 <rainbowt0506@gmail.com>
2025-01-01 22:01:38 +0800

    feat: implement song slider, duration display, and play/pause functionality
    - Add `SoundPlayerCubit` with states: loading, loaded, failure
    - Use `just_audio` library to handle audio playback
    - Implement `loadSong` and `playOrPauseSong` methods in `SoundPlayerCubit`
    - Add position and duration streams to update UI in real-time
    - Format and display song duration and position in `SongPlayerPage`
    - Create slider for song progress
    - Add play/pause button with toggling functionality
    - Integrate `SoundPlayerCubit` with UI using BlocBuilder and BlocProvider

--- lib/presentation/song_player/pages/song_player.dart
+++ lib/presentation/song_player/pages/song_player.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_guys_spotify/common/widgets/appbar/app_bar.dart';
 import 'package:flutter_guys_spotify/domain/entities/song/song.dart';
+import 'package:flutter_guys_spotify/presentation/song_player/bloc/song_player_cubit.dart';
+import 'package:flutter_guys_spotify/presentation/song_player/bloc/song_player_state.dart';
 
 import '../../../core/configs/constants/app_urls.dart';
 import '../../../core/configs/theme/app_colors.dart';
@@ -23,16 +26,25 @@ class SongPlayerPage extends StatelessWidget {
           icon: const Icon(Icons.more_vert_rounded),
         ),
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(
-            vertical: 16,
-            horizontal: 16
-        ),
-        child: Column(
-          children: [_songCover(context),
-            const SizedBox(height: 20,),
-            _songDetail()
-          ],
+      body: BlocProvider(
+        create: (_) => SongPlayerCubit()
+          ..loadSong(
+              '${AppURLs.coverFirestorage}${songEntity.artist} - ${songEntity.title}.mp3?${AppURLs.mediaAlt}'),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+          child: Column(
+            children: [
+              _songCover(context),
+              const SizedBox(
+                height: 20,
+              ),
+              _songDetail(),
+              const SizedBox(
+                height: 30,
+              ),
+              _songPlayer(context)
+            ],
+          ),
         ),
       ),
     );
@@ -40,18 +52,13 @@ class SongPlayerPage extends StatelessWidget {
 
   Widget _songCover(BuildContext context) {
     return Container(
-        height: MediaQuery
-            .of(context)
-            .size
-            .height / 2,
+        height: MediaQuery.of(context).size.height / 2,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
             image: DecorationImage(
                 fit: BoxFit.cover,
                 image: NetworkImage(
-                    '${AppURLs.coverFirestorage}${songEntity
-                        .artist} - ${songEntity.title}.jpg?${AppURLs
-                        .mediaAlt}'))));
+                    '${AppURLs.coverFirestorage}${songEntity.artist} - ${songEntity.title}.jpg?${AppURLs.mediaAlt}'))));
   }
 
   Widget _songDetail() {
@@ -63,27 +70,92 @@ class SongPlayerPage extends StatelessWidget {
           children: [
             Text(
               songEntity.title,
-              style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22
-              ),
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
             ),
-            const SizedBox(height: 5,),
+            const SizedBox(
+              height: 5,
+            ),
             Text(
               songEntity.artist,
-              style: const TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 14
-              ),
+              style: const TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
             )
           ],
         ),
-        IconButton(onPressed: (){}, icon: const Icon(
-          Icons.favorite_outline_outlined,
-          size: 35,
-          color: AppColors.darkGrey,
-        ))
+        IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.favorite_outline_outlined,
+              size: 35,
+              color: AppColors.darkGrey,
+            ))
       ],
     );
+  }
+
+  Widget _songPlayer(BuildContext context) {
+    return BlocBuilder<SongPlayerCubit, SongPlayerState>(
+      builder: (BuildContext context, SongPlayerState state) {
+        if (state is SongPlayerLoading) {
+          return const CircularProgressIndicator();
+        }
+
+        if (state is SongPlayerLoaded) {
+          return Column(
+            children: [
+              Slider(
+                  value: context
+                      .read<SongPlayerCubit>()
+                      .songPosition
+                      .inSeconds
+                      .toDouble(),
+                  min: 0.0,
+                  max: context
+                      .read<SongPlayerCubit>()
+                      .songDuration
+                      .inSeconds
+                      .toDouble(),
+                  onChanged: (value) {}),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(formatDuration(
+                      context.read<SongPlayerCubit>().songPosition)),
+                  Text(formatDuration(
+                      context.read<SongPlayerCubit>().songDuration))
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: (){
+                  context.read<SongPlayerCubit>().playOrPauseSong();
+                },
+                child: Container(
+                  height: 60,
+                  width: 60,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: AppColors.primary),
+                  child: Icon(context.read<SongPlayerCubit>().audioPlayer.playing
+                      ? Icons.pause
+                      : Icons.play_arrow),
+                ),
+              )
+            ],
+          );
+        }
+
+        return Container();
+      },
+    );
+  }
+
+  String formatDuration(Duration duration) {
+    final minutes = duration.inMinutes.remainder(60);
+    final seconds = duration.inSeconds.remainder(60);
+    return '${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
   }
 }
